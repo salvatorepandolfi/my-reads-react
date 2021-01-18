5e87262a7a629aac84a85173dc5d32cc73b08bcd
salvatore.pandolfi <salvpan@gmail.com>
2021-01-18 21:50:41 +0100

    feat: - Created shelf components, it encapsulate the books

---
+++ src/components/Shelf.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types"
+import * as BookService from '../service/BooksService'
+import Book from "./Book";
+
+class Shelf extends Component {
+
+    static propTypes = {
+        shelf: PropTypes.object.isRequired,
+        books: PropTypes.array.isRequired,
+        changeShelf: PropTypes.func.isRequired
+    }
+
+
+    render() {
+        const {books, changeShelf} = this.props
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelf.name}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.map(book => {
+                            (
+                                <li>
+                                    <Book book={book} changeShelf={changeShelf}/>
+                                </li>
+                            )
+                        })}
+                    </ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Shelf
