1f51c15b90d80b35ad33f6c76b7182bafdea0639
salvatore.pandolfi <salvpan@gmail.com>
2021-01-18 21:51:18 +0100

    fix: - removed unused import

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types"
-import * as BookService from '../service/BooksService'
 import Book from "./Book";
 
 class Shelf extends Component {
